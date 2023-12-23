8b58cd006bcc7b88184713c550fc65338d4e6c89
Bnetworx <cedric.braekevelt@live.be>
2023-12-23 21:35:46 +0100

    (article): add collation script

---
+++ ghost-blogging-on-aca/ConvertCollationAndCharset.ps1
@@ -0,0 +1,28 @@
+# Define the folder path
+$folderPath = "C:\Scripting\HB-Hybrid-Brothers-Site\Backups\MySQL-2023-10-06"
+
+# Define the values to be replaced and their replacements
+$replaceValues = @{
+    "utf8mb4" = "utf8"
+    "utf8_0900_ai_ci" = "utf8_general_ci"
+}
+
+# Get all files within the folder
+$files = Get-ChildItem -Path $folderPath -File -Recurse
+
+# Iterate through each file and perform the replacements
+foreach ($file in $files) {
+    # Read the content of the file
+    $content = Get-Content -Path $file.FullName -Raw
+
+    # Replace the values
+    foreach ($replaceValue in $replaceValues.GetEnumerator()) {
+        $content = $content -replace [regex]::Escape($replaceValue.Key), $replaceValue.Value
+    }
+
+    # Write the modified content back to the file
+    Set-Content -Path $file.FullName -Value $content
+}
+
+# Output a message when the replacements are complete
+Write-Host "Value replacements complete."
